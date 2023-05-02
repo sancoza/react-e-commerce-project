5d9163c9f08bd727e87e2d3a50ac7346160a9864
Sandra <sandrabalog@rocketmail.com>
2023-05-02 11:47:01 +0200

    added in our addToCart feature

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import './CartDropdown.style.scss';
-import {Button} from '../button/Button';
+import { Button } from '../button/Button';
+import { CartItem } from '../cart-item/CartItem';
 export const CartDropdown = () => {
-  return <div className='cart-dropdown-container '>
-    <div className='cart-items'>
-      <Button>
-        GO TO CHECKOUT
-      </Button>
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className="cart-dropdown-container ">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
     </div>
-  </div>;
+  );
 };
--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from 'react';
 import './ProductCard.style.scss';
-import {Button} from '../button/Button';
-
-export const ProductCard = ({product}) => {
-  const {imageUrl, name, price} = product;
+import { Button } from '../button/Button';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+export const ProductCard = ({ product }) => {
+  const { imageUrl, name, price } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
   return (
-    <div className='product-card-container '>
-    <img src={imageUrl} alt={`${name}`}/>
-    <div className='footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+    <div className="product-card-container ">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to Card
+      </Button>
     </div>
-    <Button buttonType='inverted'> Add to Card</Button>
-    </div>
-  )
-}
+  );
+};
